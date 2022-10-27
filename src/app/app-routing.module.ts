7ecbb35605ebcd64ce1f4090bb7f6789dc6dde3e
Antony <antony.alsberghe@hotmail.fr>
2022-10-27 14:02:33 +0200

    some modification

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ArticlesComponent } from './articles/articles.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { APIsComponent } from './apis/apis.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HelloWorldComponent },
   { path: 'article', component: ArticlesComponent },
+  { path: 'api', component: APIsComponent },
+  { path: 'contact', component: ContactComponent },
 ];
 @NgModule({
   declarations: [],
